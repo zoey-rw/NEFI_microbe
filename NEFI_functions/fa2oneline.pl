c7562b0283a3af3912598bff0940a66242cc4c90
colin.averill <colin.averill@gmail.com>
2018-08-17 15:24:38 -0400

    perl script to convert fasta files where sequence is written to multiple lines to a fasta file where the sequence is only on one line.

---
+++ NEFI_functions/fa2oneline.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#this will convert a fasta file where the sequence is written on multiple line to a fasta file where the sequence is written on one line.
+#example: perl fa2oneline.pl sample.fna > out.fna
+use strict;
+
+my $input_fasta=$ARGV[0];
+open(IN,"<$input_fasta") || die ("Error opening $input_fasta $!");
+
+my $line = <IN>; 
+print $line;
+
+while ($line = <IN>)
+{
+chomp $line;
+if ($line=~m/^>gi/) { print "\n",$line,"\n"; }
+else { print $line; }
+}
+
+print "\n";
