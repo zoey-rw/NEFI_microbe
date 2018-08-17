f170e8615c6eb4cb6a4ac78993d4b3deb09150a8
colin.averill <colin.averill@gmail.com>
2018-08-17 15:37:35 -0400

    changed perl script to simplify fasta files.

--- NEFI_functions/fa2oneline.pl
+++ NEFI_functions/fa2oneline.pl
@@ -6,14 +6,4 @@ use strict;
 my $input_fasta=$ARGV[0];
 open(IN,"<$input_fasta") || die ("Error opening $input_fasta $!");
 
-my $line = <IN>; 
-print $line;
-
-while ($line = <IN>)
-{
-chomp $line;
-if ($line=~m/^>gi/) { print "\n",$line,"\n"; }
-else { print $line; }
-}
-
-print "\n";
+perl -pe '/^>/ ? print "\n" : chomp' input_fasta | tail -n +2
