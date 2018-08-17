eb17a1d6aea7b5d1b18f9d0615173077d321a39f
colin.averill <colin.averill@gmail.com>
2018-08-17 16:05:40 -0400

    modding perl script more.

--- NEFI_functions/fa2oneline.pl
+++ NEFI_functions/fa2oneline.pl
@@ -1,9 +1,5 @@
 #!/usr/bin/perl -w
 #this will convert a fasta file where the sequence is written on multiple line to a fasta file where the sequence is written on one line.
 #example: perl fa2oneline.pl sample.fna > out.fna
-use strict;
-
-my $input_fasta=$ARGV[0];
-open(IN,"<$input_fasta") || die ("Error opening $input_fasta $!");
-
-perl -pe '/^>/ ? print "\n" : chomp' input_fasta | tail -n +2
+my $input_fasta = <STDIN>;
+perl -pe '/^>/ ? print "\n" : chomp' $input_fasta | tail -n +2;
