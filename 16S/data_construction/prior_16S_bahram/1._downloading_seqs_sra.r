e60aed75f7e09fe1c518176eca270f3fe5bc5cfb
colin.averill <colin.averill@gmail.com>
2018-09-13 15:08:02 -0400

    Merged in 16S (bacteria) scripts and paths! Also incorporated the 16S raw sequences in the NEFI_data big_data folder on the SCC. This all *should* work. Still need to update qsub paths, but first looking into building a function such that qsub paths will never need to be updated again.

---
+++ 16S/data_construction/prior_16S_bahram/1._downloading_seqs_sra.r
@@ -0,0 +1,28 @@
+#ripping Bahram et al. 2018 Nature 16S sequences from the SRA database.
+#note: this could go faster using the aspera connect tools. But this works.
+#clear environment, load packages.
+rm(list=ls())
+source('paths.r')
+#source('http://bioconductor.org/biocLite.R') #you need to install the package this way if you don't already have it.
+#biocLite('SRAdb')
+library(SRAdb)
+srafile = getSRAdbFile()
+con = dbConnect(RSQLite::SQLite(),srafile) #this takes a frickin minute. 2.3G.
+
+#Grab ftp addresses and acession numbers in case you want them later.
+d <- listSRAfile('ERP021922', con)
+#write.csv(d,'Bahram_2018_16S_accessions.csv')
+
+#Download all data to bahram sequence directory. Path is specified in paths.r
+#this is like 10G.
+getSRAfile('ERP021922', con, fileType = 'fastq',destDir = bahram.seq.dir)
+
+#unzip all fastq.gz files.
+cmd <- paste0('gunzip ',bahram.seq.dir,'/*.gz')
+system(cmd)
+
+#clean up. Remove SRAdb sqlite file.
+system('rm -f SRAmetadb.sqlite')
+system('rm -f SRAmetadb.sqlite.gz')
+
+#end script.
