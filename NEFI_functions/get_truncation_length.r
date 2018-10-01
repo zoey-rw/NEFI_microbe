7be3e894eec4fc6ee176d243d5f16233b8e1a3ff
Zoey Werbin <zoeywerbin@gmail.com>
2018-10-01 13:11:11 -0400

    wrote new function to set read truncation lengths

---
+++ NEFI_functions/get_truncation_length.r
@@ -0,0 +1,41 @@
+#' Set truncation length
+#' 
+#' Decides on truncation length for trimmed reads based on quality score means. Default cutoff is a score of 30. 
+#' Warns you if the beginning (first 10) bases are low-quality, but returns the first low-quality base after the 10th base.
+#' If no bases are below score, returns the last base.
+#' 
+#' @param fl input files (prints suggested length for each; if used in script, could take the first input, or the lowest)
+#' @param qscore default = 30.
+#' @param n default = 5e+05 don't know exactly why this matters, but it's part of how 'qa' is called within the dada2 scripts...
+#' 
+#' @export
+#' 
+#' @examples
+
+get_truncation_length <-function (fl, qscore = 30, n = 5e+05){
+  trunc_lengths <- data.frame(file = character(0), early_lowqual = numeric(0),
+                              trunc_length = numeric(0))
+  for (f in fl) {
+    srqa <- ShortRead::qa(f, n = n)
+    df <- srqa[["perCycle"]]$quality
+    means <- rowsum(df$Score * df$Count, df$Cycle)/rowsum(df$Count, 
+                                                          df$Cycle)
+    lowqual <- which(means < qscore)
+    lowqual_in_first_10 <- length(which(lowqual <= 10))
+    lowqual_after_10 <- lowqual[lowqual > 10][1]
+    
+    trunc_lengths <- rbind(trunc_lengths, data.frame(file = f, early_lowqual = lowqual_in_first_10,
+                                                    trunc_length = ifelse(is.na(lowqual_after_10), length(means), lowqual_after_10))) 
+  if(lowqual_in_first_10 > 0){
+    cat(paste0(basename(f),': ', lowqual_under_10, ' bases within first 10 bases are below your quality score. Consider trimming left.\n'))
+    } 
+  if (is.na(lowqual_after_10)){
+    cat(paste0(basename(f), ': After first 10 bases, no bases with a mean under your quality score found. Truncate at end of read, base: ', length(means),'\n'))
+  
+    } else if (!is.na(lowqual_after_10)){
+    cat(paste0(basename(f),': After first 10 bases, first mean below your quality score is base: ',lowqual_after_10,'\n'))  
+  
+    } else "Something's wrong. Inspect function/reads."
+  } # end loop
+return(trunc_lengths$trunc_length)  
+} # end function
