b295e78ba6d8dcb233115969792288702022e2ae
Zoey Werbin <zoeywerbin@gmail.com>
2018-11-05 09:58:21 -0500

    fixed rsync, sorry colin

--- transfer_scripts/rsync.sh
+++ transfer_scripts/rsync.sh
@@ -1,3 +1,7 @@
-rsync -av -e ssh /projectnb/talbot-lab-data/NEFI_data/small_data/scc_gen/ zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/small_data/scc_gen/
+rsync -av -O -e ssh /projectnb/talbot-lab-data/NEFI_data/ITS/scc_gen/ zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/ITS/scc_gen/
 
-rsync -av zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/small_data/pecan_gen/ /projectnb/talbot-lab-data/NEFI_data/small_data/pecan_gen/
+rsync -av -O  --no-p zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/ITS/pecan_gen/ /projectnb/talbot-lab-data/NEFI_data/ITS/pecan_gen/
+
+rsync -av -O -e ssh /projectnb/talbot-lab-data/NEFI_data/16S/scc_gen/ zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/16S/scc_gen/
+
+rsync -av -O --no-p zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/16S/pecan_gen/ /projectnb/talbot-lab-data/NEFI_data/16S/pecan_gen/
