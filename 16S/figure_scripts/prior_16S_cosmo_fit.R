95a575f211b99c2111a362540ccb113ed588d2ae
Zoey Werbin <zoeywerbin@gmail.com>
2018-11-15 11:50:57 -0500

    initial figure script for prior fits

---
+++ 16S/figure_scripts/prior_16S_cosmo_fit.R
@@ -0,0 +1,26 @@
+rm(list=ls())
+source('paths.r')
+library(runjags)
+library(betareg)
+source('NEFI_functions/crib_fun.r')
+
+#Check cosmo genera.
+fit <- readRDS(bahram_16S.prior_12gen_JAGSfit)
+fit <- fit$all.preds
+
+#check convergence plots and prsf scores.
+fit$species_parameter_output
+plot(fit$jags_model)
+
+#check the plots.
+par(mfrow = c(3,3))
+for(i in 1:ncol(fit$predicted)){
+  plot(fit$observed[,i]/rowSums(fit$observed) ~ fit$predicted[,i], pch = 16)
+  abline(0,1,lwd = 2)
+  abline(lm(fit$observed[,i]/rowSums(fit$observed) ~ fit$predicted[,i]), lty = 2, col = 'purple')
+  mod <- betareg::betareg(crib_fun(fit$observed[,i]/rowSums(fit$observed)) ~ crib_fun(fit$predicted[,i]))
+  rsq <-round(summary(mod)$pseudo.r.squared, 2)
+  mtext(colnames(fit$predicted)[i], side = 3)
+  mtext(paste0('R2=',rsq), side = 3, line = -1.5, adj = 0.05)
+  
+}
