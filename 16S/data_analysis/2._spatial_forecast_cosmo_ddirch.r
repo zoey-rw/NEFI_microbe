1787d122e0532468b95dd72600a03e6455638fae
Zoey Werbin <zoeywerbin@gmail.com>
2018-11-19 20:57:55 -0500

    first attempt at forecast, probably a mistake somewhere

--- 16S/data_analysis/2._spatial_forecast_cosmo_ddirch.r
+++ 16S/data_analysis/2._spatial_forecast_cosmo_ddirch.r
@@ -110,3 +110,150 @@ names(output) <- c('core.fit','plot.fit','site.fit',
                    'core.preds','plot.preds','site.preds',
                    'core.sd','plot.sd','site.sd')
 saveRDS(output, output.path)
+
+
+core.fit <- output$core.fit
+map<-readRDS("/fs/data3/caverill/NEFI_data/16S/pecan_gen/NEON_data_aggregation/obs.table_16S.rds")
+map$deprecatedVialID
+
+truth <- readRDS(NEON_cosmo_abundances.path) #UPDATE THIS FOR GENERA.
+truth <- truth$rel.abundances
+
+truth$deprecatedVialID <- rownames(truth)
+truth1 <- merge(truth, map, by = "deprecatedVialID")
+truth1 <- truth1[!duplicated(truth1$geneticSampleID),]
+rownames(truth1) <- gsub('-GEN','',truth1$geneticSampleID)
+
+
+#validate against observed data by plotting.----
+par(mfrow = c(3,1))
+trans <- 0.3
+limy <- c(0,1)
+i = 4 #3 = Russula
+
+#core.level.----
+#organize data.
+fcast <- core.fit
+truth <- readRDS(NEON_cosmo_abundances.path) #UPDATE THIS FOR GENERA.
+truth <- truth$rel.abundances
+truth$deprecatedVialID <- rownames(truth)
+truth1 <- merge(truth, map[,c("deprecatedVialID", "geneticSampleID")], by = "deprecatedVialID")
+truth1 <- truth1[!duplicated(truth1$geneticSampleID),]
+rownames(truth1) <- gsub('-GEN','',truth1$geneticSampleID)
+truth <- truth1
+truth <- truth[rownames(truth) %in% rownames(fcast$mean),]
+test <- truth[-grep('DSNY',rownames(truth)),]
+for(k in 1:length(fcast)){
+  fcast[[k]] <- fcast[[k]][rownames(fcast[[k]]) %in% rownames(truth),]
+}
+truth <- as.matrix(truth)
+mu <- fcast$mean[,i][order(fcast$mean[,i])]
+ci_0.975 <- fcast$ci_0.975[,i][order(match(names(fcast$ci_0.975[,i]),names(mu)))]
+ci_0.025 <- fcast$ci_0.025[,i][order(match(names(fcast$ci_0.025[,i]),names(mu)))]
+pi_0.975 <- fcast$pi_0.975[,i][order(match(names(fcast$pi_0.975[,i]),names(mu)))]
+pi_0.025 <- fcast$pi_0.025[,i][order(match(names(fcast$pi_0.025[,i]),names(mu)))]
+bac_name <- colnames(fcast$mean)[i]
+obs.mu   <- truth[,c(bac_name)][order(match(names(truth[,c(bac_name)]),names(mu)))]
+#obs.lo95 <- fglo95[,i][order(match(names(fglo95[,i]),names(mu)))]
+#obs.hi95 <- fghi95[,i][order(match(names(fghi95[,i]),names(mu)))]
+#plot
+plot(obs.mu ~ mu, cex = 0.7, ylim=c(0,.2), main = 'core-level')
+rsq <- round(summary(lm(obs.mu ~mu))$r.squared,2)
+mtext(paste0('R2=',rsq), side = 3, line = -2.7, adj = 0.03)
+#add confidence interval.
+range <- mu
+polygon(c(range, rev(range)),c(pi_0.975, rev(pi_0.025)), col=adjustcolor('green', trans), lty=0)
+polygon(c(range, rev(range)),c(ci_0.975, rev(ci_0.025)), col=adjustcolor('blue' , trans), lty=0)
+#fraction within 95% predictive interval.
+in_it <- round(sum(as.numeric(obs.mu) < pi_0.975 & as.numeric(obs.mu) > pi_0.025, na.rm = TRUE) / length(obs.mu),2) * 100
+state <- paste0(in_it,'% of observations within 95% prediction interval.')
+mtext(state,side = 3, line = -1.3, adj = 0.05)
+abline(0,1,lwd=2)
+
+
+
+
+#core.level.----
+#organize data.
+
+fcast <- core.fit
+truth <- readRDS(NEON_cosmo_abundances.path) #UPDATE THIS FOR GENERA.
+truth <- truth$rel.abundances
+truth$deprecatedVialID <- rownames(truth)
+truth1 <- merge(truth, map[,c("deprecatedVialID", "geneticSampleID")], by = "deprecatedVialID")
+truth1 <- truth1[!duplicated(truth1$geneticSampleID),]
+rownames(truth1) <- gsub('-GEN','',truth1$geneticSampleID)
+truth <- truth1
+truth <- truth[rownames(truth) %in% rownames(fcast$mean),]
+for(k in 1:length(fcast)){
+  fcast[[k]] <- fcast[[k]][rownames(fcast[[k]]) %in% rownames(truth),]
+}
+truth <- as.matrix(truth)
+mu <- fcast$mean[,i][order(fcast$mean[,i])]
+ci_0.975 <- fcast$ci_0.975[,i][order(match(names(fcast$ci_0.975[,i]),names(mu)))]
+ci_0.025 <- fcast$ci_0.025[,i][order(match(names(fcast$ci_0.025[,i]),names(mu)))]
+pi_0.975 <- fcast$pi_0.975[,i][order(match(names(fcast$pi_0.975[,i]),names(mu)))]
+pi_0.025 <- fcast$pi_0.025[,i][order(match(names(fcast$pi_0.025[,i]),names(mu)))]
+bac_name <- colnames(fcast$mean)[i]
+obs.mu   <- truth[,c(bac_name)][order(match(names(truth[,c(bac_name)]),names(mu)))]
+#obs.lo95 <- fglo95[,i][order(match(names(fglo95[,i]),names(mu)))]
+#obs.hi95 <- fghi95[,i][order(match(names(fghi95[,i]),names(mu)))]
+#plot
+plot(obs.mu ~ mu, cex = 0.7, ylim=c(0,.05), main = 'core-level')
+rsq <- round(summary(lm(obs.mu ~mu))$r.squared,2)
+mtext(paste0('R2=',rsq), side = 3, line = -2.7, adj = 0.03)
+#add confidence interval.
+range <- mu
+polygon(c(range, rev(range)),c(pi_0.975, rev(pi_0.025)), col=adjustcolor('green', trans), lty=0)
+polygon(c(range, rev(range)),c(ci_0.975, rev(ci_0.025)), col=adjustcolor('blue' , trans), lty=0)
+#fraction within 95% predictive interval.
+in_it <- round(sum(as.numeric(obs.mu) < pi_0.975 & as.numeric(obs.mu) > pi_0.025) / length(obs.mu),2) * 100
+state <- paste0(in_it,'% of observations within 95% prediction interval.')
+mtext(state,side = 3, line = -1.3, adj = 0.05)
+abline(0,1,lwd=2)
+
+
+
+
+
+
+
+#site.level----
+#organize data.
+fcast <- output$site.fit
+truth <- readRDS(NEON_site.level_genera_obs_16S.path)
+for(k in 1:length(truth)){
+  rownames(truth[[k]]) <- gsub('.','_',rownames(truth[[k]]), fixed = T)
+  truth[[k]] <- truth[[k]][rownames(truth[[k]]) %in% rownames(fcast$mean),]
+}
+#drop DSNY
+for(k in 1:length(truth)){
+  truth[[k]] <- truth[[k]][-(grep('DSNY',rownames(truth[[k]]))),]
+}
+for(k in 1:length(fcast)){
+  fcast[[k]] <- fcast[[k]][rownames(fcast[[k]]) %in% rownames(truth$mean),]
+}
+mu <- fcast$mean[,i][order(fcast$mean[,i])]
+ci_0.975 <- fcast$ci_0.975[,i][order(match(names(fcast$ci_0.975[,i]),names(mu)))]
+ci_0.025 <- fcast$ci_0.025[,i][order(match(names(fcast$ci_0.025[,i]),names(mu)))]
+pi_0.975 <- fcast$pi_0.975[,i][order(match(names(fcast$pi_0.975[,i]),names(mu)))]
+pi_0.025 <- fcast$pi_0.025[,i][order(match(names(fcast$pi_0.025[,i]),names(mu)))]
+bac_name <- colnames(fcast$mean)[i]
+obs.mu   <- truth$mean[,bac_name][order(match(names(truth$mean[,bac_name]),names(mu)))]
+obs.lo95 <- truth$lo95[,bac_name][order(match(names(truth$lo95[,bac_name]),names(mu)))]
+obs.hi95 <- truth$hi95[,bac_name][order(match(names(truth$hi95[,bac_name]),names(mu)))]
+#plot
+plot(obs.mu ~ mu, cex = 0.7, ylim=c(0,.1), main='site-level')
+arrows(c(mu), obs.lo95, c(mu), obs.hi95, length=0.05, angle=90, code=3)
+rsq <- round(summary(lm(obs.mu ~mu))$r.squared,2)
+mtext(paste0('R2=',rsq), side = 3, line = -2.7, adj = 0.03)
+#1:1 line
+abline(0,1, lwd = 2)
+#add confidence interval.
+range <- mu
+polygon(c(range, rev(range)),c(pi_0.975, rev(pi_0.025)), col=adjustcolor('green', trans), lty=0)
+polygon(c(range, rev(range)),c(ci_0.975, rev(ci_0.025)), col=adjustcolor('blue' , trans), lty=0)
+#fraction within 95% predictive interval.
+in_it <- round(sum(obs.mu < pi_0.975 & obs.mu > pi_0.025) / length(obs.mu),2) *100
+state <- paste0(in_it,'% of observations within 95% prediction interval.')
+mtext(state,side = 3, line = -1.3, adj = 0.05)
