e49a1865d039a0d00041d73b25b71825d6a29bdc
Zoey Werbin <zoeywerbin@gmail.com>
2018-12-04 17:22:03 -0500

    fixed column name

--- 16S/data_analysis/prior16S-bahram_phylo.group_ddirch_fit.r
+++ 16S/data_analysis/prior16S-bahram_phylo.group_ddirch_fit.r
@@ -23,12 +23,13 @@ registerDoParallel(cores=n.cores)
 #set output path.----
 output.path <- bahram_16S_prior_phylo.group_JAGSfits
 
-#load tedersoo data.----
+#load bahram data.----
 d <- data.table(readRDS(bahram_metadata.path))
 y <- readRDS(bahram_16S_common_phylo_groups_list.path)
 d <- d[,.(Run,pC,cn,PH,NPP,map,mat,forest,conifer,relEM)]
 d <- d[complete.cases(d),] #optional. This works with missing data.
-#d <- d[1:35,] #for testing
+d <- d[1:5,] #for testing
+
 
 #Drop in intercept, setup predictor matrix.
 x <- d
@@ -49,10 +50,10 @@ output.list<-
   foreach(i = 1:length(y)) %dopar% {
     y.group <- y[[i]]
     y.group <- y.group$abundances
-    y.group <- y.group[rownames(y.group) %in% d$SRR.id,]
+    y.group <- y.group[rownames(y.group) %in% d$Run,]
     y.group <- y.group + 1
     y.group <- y.group/rowSums(y.group)
-    if(!sum(rownames(y.group) == d$SRR.id) == nrow(y.group)){
+    if(!sum(rownames(y.group) == d$Run) == nrow(y.group)){
       cat('Warning. x and y covariates not in the same order!')
     }
     fit <- site.level_dirlichet_jags(y=y.group,x_mu=x,
