a43e05f4d01af1826e9f14a7ba2091c04150e4a9
Zoey Werbin <zoeywerbin@gmail.com>
2019-02-15 16:32:21 -0500

    moving chmod command earlier in cron script

--- transfer_scripts/rsync.sh
+++ transfer_scripts/rsync.sh
@@ -1,8 +1,5 @@
 # script for rsync between pecan2 and scc data directories.
 
-# set permissions to read/write/execute for everyone.
-chmod 777 -R /projectnb/talbot-lab-data/NEFI_data/
-
 # sending ITS data from SCC to pecan
 rsync -av -O -e ssh /projectnb/talbot-lab-data/NEFI_data/ITS/scc_gen/ zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/ITS/scc_gen/
 
@@ -14,3 +11,6 @@ rsync -av -O -e ssh /projectnb/talbot-lab-data/NEFI_data/16S/scc_gen/ zrwerbin@p
 
 # sending 16S data from pecan to SCC
 rsync -av -O zrwerbin@pecan2.bu.edu:/fs/data3/caverill/NEFI_data/16S/pecan_gen/ /projectnb/talbot-lab-data/NEFI_data/16S/pecan_gen/
+
+# set permissions to read/write/execute for everyone.
+chmod 777 -R /projectnb/talbot-lab-data/NEFI_data/
