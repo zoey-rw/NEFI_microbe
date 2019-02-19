363813247ad43f1775794fc92fa28071c0bfb216
Zoey Werbin <zoeywerbin@gmail.com>
2019-02-19 16:54:08 -0500

    testing only one cov selection fit

--- 16S/spatial_analysis/cov_selection_bahram_16S_prior_fg.r
+++ 16S/spatial_analysis/cov_selection_bahram_16S_prior_fg.r
@@ -14,7 +14,6 @@ d <- d[complete.cases(d),] #optional. This works with missing data.
 
 output.path <- bahram_16S_prior_fg_cov.selection_JAGS
 
-
 # load Bahram functional group abundances
 a1 <- readRDS(prior_N_cyclers_abundances.path)
 a2 <- readRDS(prior_C_cyclers_abundances.path)
@@ -23,6 +22,13 @@ a3 <- list(readRDS(prior_cop_olig_abundances.path))
 # combine these three lists of lists - just get abundances
 a <- do.call(c, list(a1, a2, a3))
 fg_all <- sapply(a, "[[", 1)
+
+
+#Drop in intercept, setup predictor matrix.
+d$intercept <- rep(1,nrow(d))
+d$map <- log(d$map)
+x <- as.data.frame(d[,.(intercept,pC,cn,pH,Ca,Mg,P,K,moisture,NPP,map,mat,forest,conifer,relEM)])
+x.no.nutr <- as.data.frame(d[,.(intercept,pC,cn,pH,moisture,NPP,map,mat,forest,conifer,relEM)])
 
 #fg_all <- c("N_cyclers", "C_cyclers", "Cop_olig")
 all_fg_output <- list()
@@ -47,21 +53,17 @@ for (f in 1:length(fg_all)) {
   y <- y/rowSums(y)
   y <- as.data.frame(y)
   
-  #Drop in intercept, setup predictor matrix.
-  d$intercept <- rep(1,nrow(d))
-  d$map <- log(d$map)
-  x <- as.data.frame(d[,.(intercept,pC,cn,pH,Ca,Mg,P,K,moisture,NPP,map,mat,forest,conifer,relEM)])
-  x.no.nutr <- as.data.frame(d[,.(intercept,pC,cn,pH,moisture,NPP,map,mat,forest,conifer,relEM)])
-  
-  covs <- covariate_selection_JAGS(y=y,x_mu=x, n.adapt = 300, n.burnin = 1000, 
-                                   n.sample = 1000,parallel = F)
-  cat(paste0("First covariate selection complete for group: ", colnames(y)[2]))
+  # covs <- covariate_selection_JAGS(y=y,x_mu=x, n.adapt = 300, n.burnin = 1000, 
+  #                                  n.sample = 1000,parallel = F)
+  # cat(paste0("First covariate selection complete for group: ", colnames(y)[2]))
   covs.no.nutr <- covariate_selection_JAGS(y=y,x_mu=x.no.nutr, n.adapt = 300, n.burnin = 1000, 
                                            n.sample = 1000, parallel = F)
   cat(paste0("Second (no.nutr) covariate selection complete for group: ", colnames(y)[2]))
   
-  fg_output <- list(covs, covs.no.nutr)
-  names(fg_output) <- c(paste0("Cov_select ", colnames(y)[2]), paste0("No.nutr cov_select ", colnames(y)[2]))
+  fg_output <- list(#covs, 
+                    covs.no.nutr)
+  names(fg_output) <- c(#paste0("Cov_select ", colnames(y)[2]), 
+                        paste0("No.nutr cov_select ", colnames(y)[2]))
   cat(paste0("All covariate selection complete for group: ", colnames(y)[2]))
   all_fg_output[[f]] <- fg_output
   #return(fg_output)
@@ -77,11 +79,6 @@ for (i in 1:12) {
 group_names[[12]] <- "Cop_olig" #Cop_olig has one more column than the other 11 
 # add names to each item
 names(all_fg_output) <- group_names
-# names(covs) <- c("N_cycling","C_cycling","Cop_olig")
-# names(covs[[1]]) <- c("Assim_nitrite_reduction", "Dissim_nitrite_reduction", "Assim_nitrate_reduction", 
-#                       "N_fixation", "Dissim_nitrate_reduction", "Nitrification", "Denitrification")
-# names(covs[[2]]) <- c("Cellulolytic", "Chitinolytic", "Lignolytic", "Methanotroph")
-# names(covs[[3]]) <- "Cop_olig"
 
 # save the output
 saveRDS(all_fg_output, bahram_16S_prior_fg_cov.selection_JAGS)
